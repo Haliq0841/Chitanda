ce3baccd08466903b0136726b1c3f6e72337c3d7
Haliq0841 <131313850+Haliq0841@users.noreply.github.com>
2025-11-14 19:02:46 +0800

    Update chitanda_gc.js

--- plugins/after/chitanda_gc.js
+++ plugins/after/chitanda_gc.js
@@ -1,7 +1,7 @@
 let handler = m => m
 handler.after = async function (m) {
     if (m.limit) {
-        if (m.from === '120363041768083672@g.us') {
+        if (m.from == '120363041768083672@g.us' || m.from == '120363025321921305@g.us') {
             m.limit = m.limit * -1
         }
     }
